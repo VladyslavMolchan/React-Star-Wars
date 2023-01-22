f613c966c8efe5e66283369f6b7ed631b6d84dca
Vlad <Nksb2017@gmail.com>
2023-01-22 13:13:48 +0200

    no comments

---
+++ src/utils/network.js
@@ -0,0 +1,25 @@
+const SWAPI_ROOT ='https://swapi.dev/api/';
+const SWAPI_PEOPLE = 'people';
+export const getApiResours = async (url) => {
+    try {
+        const res = await fetch(url);
+
+            if (!res.ok) {
+                console.error('Ошибка', res.status);
+                return false;
+            }
+        return await res.json();
+    } catch (error) {
+        console.error(error.message)
+        return false;
+    }
+}
+
+(async () => {
+    const body = await getApiResours(SWAPI_ROOT + SWAPI_PEOPLE);
+    console.log(body)
+})();
+
+// I can use promise as well
+//getApiResours(SWAPI_ROOT + SWAPI_PEOPLE)
+   // .then(body => console.log(body))
