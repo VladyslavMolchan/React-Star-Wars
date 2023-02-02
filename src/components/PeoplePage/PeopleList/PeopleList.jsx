ed62b9a7575d6bf7f456f8e2f7f7cb1a25613c2a
Vlad <Nksb2017@gmail.com>
2023-02-02 18:20:49 +0200

    add css styles in PeopleList.module.css

--- src/components/PeoplePage/PeopleList/PeopleList.jsx
+++ src/components/PeoplePage/PeopleList/PeopleList.jsx
@@ -5,8 +5,10 @@ const PeopleList = ({ people }) => {
         <ul className={styles.list__container}>
             {people.map(({name, id, img}) =>
                 <li className={styles.list__item} key={id}>
-                    <img className={styles.person__photo} src={img} alt={name}/>
-                    <p>{name}</p>
+                    <a href='#'>
+                        <img className={styles.person__photo} src={img} alt={name}/>
+                        <p>{name}</p>
+                    </a>
                 </li>
             )}
         </ul>
